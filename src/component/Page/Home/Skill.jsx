8ecf27154c506b7a40de0a9e95421e37819f3e22
Programing Hero <sajalb1899@gmail.com>
2023-12-12 19:52:12 +0600

    skill section added

--- src/component/Page/Home/Skill.jsx
+++ src/component/Page/Home/Skill.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 
 const Skill = () => {
+    const [data , setData]=useState([])
+    useEffect(()=>{
+        fetch('../../../../public/skill.json')
+        .then(res => res.json())
+        .then(data => setData(data))
+    },[])
     return (
         <div>
             <h2 className="text-5xl font-bold mt-10 text-center max-w-min m-auto bg-clip-text text-transparent bg-gradient-to-r from-cyan-500  to-pink-500">Skill</h2>
             <hr className="border-2 w-52 mt-3 m-auto border-red-500" />
+            <div className="grid grid-cols-4">
+            {
+                data?.map(skill => <div key={skill.id}>
+                    <div>
+                        <p>{skill.name}</p>
+                        <img src={skill.image} alt="" />
+                    </div>
+                </div>)
+            }
+            </div>
         </div>
     );
 };

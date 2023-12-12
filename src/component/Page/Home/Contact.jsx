16df73836c3a0bc1b93f2f0462a6b40a9120499d
Programing Hero <sajalb1899@gmail.com>
2023-12-12 11:01:01 +0600

    form submit & recive my email

--- src/component/Page/Home/Contact.jsx
+++ src/component/Page/Home/Contact.jsx
@@ -7,14 +7,14 @@ const Contact = () => {
         <h2 className="text-center text-5xl font-bold mt-4 mb-4">Get in touch</h2>
         <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <div>
-        <form className="card-body border border-black md:max-w-3xl m-auto lg:max-w-5xl mt-10">
-        <div className="grid grid-cols-2 gap-5">
-          <input type="text" placeholder="Name *" className="px-3 py-2 outline-none border border-black placeholder-slate-950" required />
-          <input type="email" placeholder="Email *" className="px-3 py-2 outline-none border border-black placeholder-slate-950" required />
+        <form className="card-body border border-black md:max-w-3xl md:m-auto lg:max-w-5xl mt-10 rounded-xl m-3"  action="https://formspree.io/f/mqkvrnol" method="POST">
+        <div className="md:grid grid-cols-2 gap-5">
+          <input type="name" name="Name" placeholder="Name *" className="px-3 py-2 mt-2 outline-none border border-black placeholder-slate-950 w-full" required />
+          <input type="email" name="Email" placeholder="Email *" className="px-3 py-2 mt-2 outline-none border border-black placeholder-slate-950 w-full" required />
         </div>
         <div className="form-control">
-          <input type="text" placeholder="Subject *"  className="px-3 py-2 outline-none border border-black placeholder-slate-950" required /> 
-          <textarea name="" id="" cols="30" rows="10" placeholder="Text *" className="border outline-none placeholder-slate-950 px-3 py-1 border-black mt-2 " required></textarea>
+          <input type="text" name="Subject" placeholder="Subject *"  className="px-3 py-2 outline-none border border-black placeholder-slate-950" required /> 
+          <textarea name="Description" id="" cols="30" rows="10" placeholder="Text *" className="border outline-none placeholder-slate-950 px-3 py-1 border-black mt-2 " required></textarea>
         </div>
         <div className="form-control mt-6">
           <button className="text-left font-bold text-xl w-36 hover:border-2 border-2 border-b-white hover:border-b-rose-400">Send Massage</button>
